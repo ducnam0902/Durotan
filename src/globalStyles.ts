2e86a101c537bf335870b2653190889c068b7edd
unknown <ducnam0902@gmail.com>
2023-04-30 21:26:53 +0700

    Base | Config for UI library and state management

---
+++ src/globalStyles.ts
@@ -0,0 +1,68 @@
+export const theme = {
+  styles: {
+    global: {
+      'html, body': {
+        fontFamily: 'Jost, sans-serif',
+        fontSize: '18px',
+        fontWeight: '400',
+        lintHeight: '1.67',
+        wordWrap: 'break-word',
+        color: '#666666',
+      },
+      colors: {
+        darker: '#000000',
+        dark: '#222222',
+        light: '#ffffff',
+        gray: '#999999',
+        primary: '#928656',
+        blue: '#00ccff',
+        backgroundDarker: '#000000',
+        backgroundLight: '#ffffff',
+        backgroundGray: '#999999',
+        backgroundPrimary: '#928656',
+        textInput: '#999999',
+        textMain: '#666666',
+        textInputFocus: '#010101',
+        linkColor: '#000000',
+        borderPrimary: '#928656',
+        borderLight: '#ffffff',
+        borderInput: '#d8d8d8',
+        borderInputFocus: '#000000',
+      },
+      'h1, h2, h3, h4, h5, h6': {
+        fontWeight: '400',
+        color: '#000000',
+        fontFamily: 'Marcellus, sans-serif',
+      },
+      h1: {
+        fontSize: '60px',
+      },
+      h2: {
+        fontSize: '48px',
+      },
+      h3: {
+        fontSize: '36px',
+      },
+      h4: {
+        fontSize: '30px',
+      },
+      h5: {
+        fontSize: '24px',
+      },
+      h6: {
+        fontSize: '20px',
+      },
+      a: {
+        color: '#000000',
+        textDecoration: 'none',
+      },
+      'a:focus, a:hover, a:active': {
+        outline: 0,
+        color: '#928656',
+      },
+      p: {
+        marginBottom: '1.5em',
+      },
+    },
+  },
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import './index.css';
-
+import { ChakraProvider } from '@chakra-ui/react';
+import { theme } from './globalStyles.ts';
+import '@fontsource/jost';
+import '@fontsource/marcellus';
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
 );
