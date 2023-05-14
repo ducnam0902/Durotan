afb484480d00d78c689d3d6b3a72a5c2803aab11
nam <70477669+ducnam0902@users.noreply.github.com>
2023-05-14 05:01:34 +0000

    config| config unit test with vitest and react testing library (#9)

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { ChakraProvider } from '@chakra-ui/react';
 import { theme } from './globalStyles.ts';
-import { BrowserRouter } from 'react-router-dom';
 
 import '@fontsource/jost';
 import '@fontsource/marcellus';
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ChakraProvider theme={theme}>
-        <App />
-      </ChakraProvider>
-    </BrowserRouter>
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
 );
